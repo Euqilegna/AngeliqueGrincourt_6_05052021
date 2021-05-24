48474c6e06bfab38d0bd88d3ecdbf3d8c0addc1f
Angelique Grincourt <angelique.grnt@gmail.com>
2021-05-24 16:58:30 +0200

    feat: gestion like/dislike

--- Backend/controllers/sauces.js
+++ Backend/controllers/sauces.js
@@ -1,7 +1,7 @@
 //logique métier
 
 const sauceModel = require("../models/sauces");
-const fs = require('fs')
+const fs = require("fs");
 
 exports.getSauces = (req, res, next) => {
   sauceModel
@@ -48,12 +48,82 @@ exports.modifySauce = (req, res, next) => {
       }
     : { ...req.body };
   sauceModel
-    .updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .updateOne({ _id: req.params.id }, { ...sauceObject })
     .then(() => res.status(200).json({ message: "Objet modifié !" }))
     .catch((error) => res.status(400).json({ error }));
 };
 
-exports.sauceState = (req, res, next) => {};
+exports.sauceState = (req, res, next) => {
+  sauceModel
+    .findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const indexLike = sauce.usersLiked.findIndex(
+        (e) => e === req.body.userId
+      );
+      const indexDislike = sauce.usersDisliked.findIndex(
+        (e) => e === req.body.userId
+      );
+
+      switch (req.body.like) {
+        case 1: // Mon user like la sauce
+          if (indexLike <= -1) {
+            // On vérifie quand même (même si le cas ne doit pas arriver avec le front) qu'il n'est pas présent dans le tableau (éviter les doublons)
+            sauce.usersLiked.push(req.body.userId);
+            sauce.likes += 1;
+          }
+          if (indexDislike > -1) {
+            // On vérifier si l'utilisateur est présent dans le tableau des dislikes
+            sauce.usersDisliked.splice(indexDislike, 1);
+            sauce.dislikes -= 1;
+          }
+          break;
+        case 0:
+          if (indexLike > -1) {
+            // On vérifie quand même (même si le cas ne doit pas arriver avec le front) qu'il n'est pas présent dans le tableau (éviter les doublons)
+            sauce.usersLiked.splice(indexLike, 1);
+            sauce.likes -= 1;
+          }
+
+          if (indexDislike > -1) {
+            // On vérifier si l'utilisateur est présent dans le tableau des dislikes
+            sauce.usersDisliked.splice(indexDislike, 1);
+            sauce.dislikes -= 1;
+          }
+          break;
+        case -1:
+          if (indexLike > -1) {
+            // On vérifie quand même (même si le cas ne doit pas arriver avec le front) qu'il n'est pas présent dans le tableau (éviter les doublons)
+            sauce.usersLiked.splice(indexLike, 1);
+            sauce.likes -= 1;
+          }
+          if (indexDislike <= -1) {
+            // On vérifier si l'utilisateur est présent dans le tableau des dislikes
+            sauce.usersDisliked.push(req.body.userId);
+            sauce.dislikes += 1;
+          }
+
+          break;
+      }
+
+      console.log("sauce", sauce);
+
+      sauceModel
+        .updateOne(
+          { _id: req.params.id },
+          {
+            likes: sauce.likes,
+            dislikes: sauce.dislikes,
+            usersLiked: sauce.usersLiked,
+            usersDisliked: sauce.usersDisliked,
+          }
+        )
+        .then((result) => {
+          result ? res.status(200).json(result) : res.status(401).json(null);
+        })
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.deleteSauce = (req, res, next) => {
   sauceModel
@@ -68,14 +138,16 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 exports.deleteSauce = (req, res, next) => {
-  sauceModel.findOne({ _id: req.params.id })
-    .then(thing => {
-      const filename = thing.imageUrl.split('/images/')[1];
+  sauceModel
+    .findOne({ _id: req.params.id })
+    .then((thing) => {
+      const filename = thing.imageUrl.split("/images/")[1];
       fs.unlink(`images/${filename}`, () => {
-        sauceModel.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-          .catch(error => res.status(400).json({ error }));
+        sauceModel
+          .deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+          .catch((error) => res.status(400).json({ error }));
       });
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }));
 };
